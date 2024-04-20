2a95f9f83d336ec671c525d17eb1f10739feef07
Piotr Menel <pmenel@inpost.pl>
2024-04-20 10:27:23 +0200

    done game works

--- rps.js
+++ rps.js
@@ -15,11 +15,32 @@ function computerChoice() {
     return computersChoice;
 }
 
-let playerSelection = playerChoice().toUpperCase();
-let computerSelection = computerChoice().toUpperCase();
-console.log(playerSelection);
-console.log(computerSelection);
-
 function playRound(playerSelection, computerSelection) {
-    
+    if (playerSelection === computerSelection) {
+        let message = "it's a tie";
+        console.log(message);
+        return message
+    } else if (
+        (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') ||
+        (playerSelection === 'PAPER' && computerSelection === 'ROCK') ||
+        (playerSelection === 'SCISSORS' && computerSelection === 'PAPER')
+    ) {
+        let message = "player won";
+        console.log(message);
+        return message
+    } else {
+        let message = "player lost";
+        console.log(message);
+        return message
+    }
 }
+
+function playGame(params) {
+    let playerSelection = playerChoice().toUpperCase();
+    let computerSelection = computerChoice().toUpperCase();
+    console.log(playerSelection);
+    console.log(computerSelection);
+    playRound(playerSelection, computerSelection)
+}
+
+playGame();
